23ab3fa5f6b331c10ead4b5132c4a6e5647a7881
Gava97 <gava97@gmail.com>
2014-07-29 22:02:30 +0200

    Update recovery.mk

--- recovery/recovery.mk
+++ recovery/recovery.mk
@@ -1,9 +1,8 @@
 # Recovery resources
 PRODUCT_COPY_FILES += \
-    bootable/recovery/res/images/icon_clockwork.png:root/res/images/icon_clockwork.png.png \
+    bootable/recovery/res/images/icon_clockwork.png:root/res/images/icon_clockwork.png \
     bootable/recovery/res/images/icon_error.png:root/res/images/icon_error.png \
     bootable/recovery/res/images/icon_firmware_error.png:root/res/images/icon_firmware_error.png \
-    bootable/recovery/res/images/icon_error.png:root/res/images/icon_error.png \
     bootable/recovery/res/images/icon_firmware_install.png:root/res/images/icon_firmware_install.png \
     bootable/recovery/res/images/icon_installing.png:root/res/images/icon_installing.png \
     bootable/recovery/res/images/indeterminate1.png:root/res/images/indeterminate1.png \
@@ -12,12 +11,6 @@ PRODUCT_COPY_FILES += \
     bootable/recovery/res/images/indeterminate1.png:root/res/images/indeterminate4.png \
     bootable/recovery/res/images/indeterminate1.png:root/res/images/indeterminate5.png \
     bootable/recovery/res/images/indeterminate1.png:root/res/images/indeterminate6.png \
-    bootable/recovery/res/images/indeterminate01.png:root/res/images/indeterminate01.png \
-    bootable/recovery/res/images/indeterminate02.png:root/res/images/indeterminate02.png \
-    bootable/recovery/res/images/indeterminate03.png:root/res/images/indeterminate03.png \
-    bootable/recovery/res/images/indeterminate04.png:root/res/images/indeterminate04.png \
-    bootable/recovery/res/images/indeterminate05.png:root/res/images/indeterminate05.png \
-    bootable/recovery/res/images/indeterminate06.png:root/res/images/indeterminate06.png \
     bootable/recovery/res/images/progress_empty.png:root/res/images/progress_empty.png \
     bootable/recovery/res/images/progress_fill.png:root/res/images/progress_fill.png \
 
