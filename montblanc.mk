eee36396ab3155599b592ad87f248690578c514a
Gava97 <gava97@gmail.com>
2014-07-30 13:42:15 +0200

    Update montblanc.mk

--- montblanc.mk
+++ montblanc.mk
@@ -107,4 +107,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \    
 	ro.debuggable=1 \
 	persist.service.adb.enable=1 \
-	persist.service.debuggable=1 \
+	persist.service.debuggable=1
